fbc71a2653a0a5409ccfc1577a99473bd59ecb60
jameszymoncome <zymoncome14@gmail.com>
2024-07-01 14:30:55 +0800

    title change

--- src/StartPage.js
+++ src/StartPage.js
@@ -7,8 +7,7 @@ function StartPage({ onStart }) {
   return (
     <div className="start-page-container">
       <img src={logo} alt="Logo" className="logo" />
-      <h1>Camarines Norte State College
-      Customer Feedback System</h1>
+      <h1>TANGINAMO JYP</h1>
       <button onClick={onStart}>Start</button>
     </div>
   );
